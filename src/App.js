31ecb6d6e3d58fac462bba6881faea595cde0c20
pwadeveloper <sirmudiadavid@gmail.com>
2018-04-11 17:31:16 +0200

    My first commit

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import Note from './components/note';
+import './App.css';
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      noteText:'',
+      notes: [],
+    }
+  }
+
+  updateNoteText(noteText){
+    this.setState({noteText: noteText.target.value})
+    console.log(this.state.noteText);
+  }
+
+  addNote(){
+    if (this.state.noteText === '') {return}
+
+    let notesArr = this.state.notes;
+    notesArr.push(this.state.noteText);
+    this.setState({noteText: ''})
+    this.textInput.focus();
+  }
+
+  handleKeyPress = (event) =>{
+    if (event.key === 'Enter'){
+      this.addNote()
+      console.log(this.event);
+    }
+    
+  }
+
+  deleteNote(index){
+    let notesArr = this.state.notes;
+    notesArr.splice(index, 1)
+    this.setState({notes: notesArr})
+  }
+
+  render() {
+    let notes = this.state.notes.map((val, key) => {
+      return <Note key ={key} text={val}
+        deleteMethod = { ()=> this.deleteNote(key)}/>
+    })
+
+    return (
+      <div className="container">
+         <div className="header">Note Snippets</div>
+         {notes}
+         
+         <div className="btn" onClick={this.addNote.bind(this)}>+</div>
+
+        <input type="text" ref={((input) =>{this.textInput = input})}
+        className="textInput"
+        value={this.state.noteText}
+        onChange={noteText => this.updateNoteText(noteText)}
+        onKeyPress={this.handleKeyPress.bind(this)}/>
+      </div>
+    );
+  }
+}
+
+export default App;
